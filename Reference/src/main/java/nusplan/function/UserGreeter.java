b9cc4b6c6b3792fd9cdb3ffd350c713a01e0f9c0
jerroldlam <jerroldlam@github.com>
2020-09-05 10:19:22 +0800

    Refactor code into nusPLAN
    -Add gitignore
    -Add greeting functions

---
+++ Reference/src/main/java/nusplan/function/UserGreeter.java
@@ -0,0 +1,18 @@
+package nusplan.function;
+
+public class UserGreeter {
+
+    /**
+     * Prints welcome message for User during entry.
+     */
+    public static void showWelcomeMessage() {
+        System.out.println("-----------Welcome to nusPLAN!-------------------");
+    }
+
+    /**
+     * Prints exit message for User during exit.
+     */
+    public static void showExitMessage() {
+        System.out.println("----------Thanks for using nusPLAN!--------------");
+    }
+}
