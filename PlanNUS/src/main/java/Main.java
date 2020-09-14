1cf852796983c7b1b0b48f4e467c850db9fca747
Khenus <khenus.tan@gmail.com>
2020-09-15 00:25:22 +0800

    V0.8 Combined Zi Hao with master

--- PlanNUS/src/main/java/Main.java
+++ PlanNUS/src/main/java/Main.java
@@ -1,49 +1,35 @@
 import model.Person;
-import moduledata.ModuleInitializer;
-
 import model.CAPlist;
-import moduledata.ModuleDatum;
 import moduledata.ModuleInitializer;
 
-import java.text.DecimalFormat;
-import java.math.RoundingMode;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        final String ERROR_INVALID_COMMAND = "Sorry, invalid command";
+
         ModuleInitializer allModules =  new ModuleInitializer();
         Person Bob = new Person("Bob" , 3, allModules);
 
-//        System.out.println("Welcome to PlanNUS! Apps available are:\n" +
-//                "  add <module code>\n" +
-//                "  edit <module code>\n" +
-//                "  remove <module code>\n" +
-//                "Type a command to continue...");
-//        Scanner scanner = new Scanner(System.in);
-//        String fullInput = scanner.nextLine().toUpperCase();
-//        String[] inputs = fullInput.split(" ");
-//
-//        while (!inputs[0].equals("EXIT")) {
-//            if (inputs[0].equals("ADD")) {
-//                addModule(scanner, inputs[1]);
-//            } else if (inputs[0].equals("EDIT")) {
-//                editModule(scanner, inputs[1]);
-//            } else if (inputs[0].equals("REMOVE")) {
-//                removeModule(inputs[1]);
-//            } else {
-//                System.out.println(ERROR_INVALID_COMMAND);
-//            }
-//
-//            fullInput = scanner.nextLine().toUpperCase();
-//            inputs = fullInput.split(" ");
-//        }
+        System.out.println("Welcome to PlanNUS! Apps available are:\n" +
+                "  Module Tracker (Use Command: modTrack)\n" +
+                "  CAP Calculator (Use Command: capCalc)\n" +
+                "To exit PlanNUS, use command: \"exit\"\n" +
+                "Type a command to continue...");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine().toLowerCase();
 
-        Bob.moduleTracker();
+        while (!input.startsWith("exit")) {
+            if (input.startsWith("modtrack")) {
+                Bob.moduleTracker();
+            } else if (input.startsWith("capcalc")) {
+                CAPlist capList = new CAPlist();
+                capList.CAPCalculator();
+            } else {
+                System.out.println(ERROR_INVALID_COMMAND);
+            }
 
-        CAPlist CAPlist = new CAPlist();
-        CAPlist.setInitialCAP();
-        CAPlist.setCurrentCAP();
-        CAPlist.setTargetCAP();
-//        CAPlist.setSUs(); WORK IN PROCESS!!
+            input = scanner.nextLine().toLowerCase();
+        }
     }
 }
