d5f2470df456ff4eb02d2e705143bbc13eed12f3
Khenus <khenus.tan@gmail.com>
2020-09-17 11:24:31 +0800

    V0.9 Updated Index.js to have checker

--- DataFormatter/JsonFormatter/index.js
+++ DataFormatter/JsonFormatter/index.js
@@ -49,101 +49,121 @@
 
 import {details} from './moduleInfo.js';
 import {list} from './moduleList.js';
+import {data} from './data.js';
 import fs from 'fs';
 
-var templateObj = {
-  moduleCode: "",
-  title: "",
-  moduleCredit: "",
-  department: "",
-  faculty: "",
-  semester: [],
-  preclusion: "",
-  attibutes: [false, false, false, false, false, false, false, false, false],
-  prerequisite: "",
-  corequisite: "",
+var generation = false;
+var checking = true;
+
+if (generation === true) {
+  var templateObj = {
+    moduleCode: "",
+    title: "",
+    moduleCredit: "",
+    department: "",
+    faculty: "",
+    semester: [],
+    preclusion: "",
+    attibutes: [false, false, false, false, false, false, false, false, false],
+    prerequisite: "",
+    corequisite: "",
+  }
+  
+  var finalArr = [];
+  
+  for (var i = 0; i < details.length; i++ ){
+    // console.log(details[i]);
+    var newObj = {...templateObj};
+  
+    newObj.moduleCode = details[i].moduleCode;
+    newObj.title = details[i].title;
+    newObj.moduleCredit = parseInt(details[i].moduleCredit);
+    newObj.department = details[i].department;
+    newObj.faculty = details[i].faculty;
+    
+    if (details[i].preclusion !== undefined) {
+      newObj.preclusion = details[i].preclusion;
+    }
+  
+    if (details[i].prerequisite !== undefined) {
+      newObj.prerequisite = details[i].prerequisite;
+    }
+    
+    if (details[i].corequisite !== undefined) {
+      newObj.corequisite = details[i].corequisite;
+    }
+    
+    if (details[i].attibutes !== undefined) {
+      for (var k = 0; k < details[i].attibutes.length; k++) {
+        if (details[i].attibutes[k].su === true) {
+          newObj.attibutes[0] = true;
+        }
+    
+        if (details[i].attibutes[k].sfs === true) {
+          newObj.attibutes[1] = true;
+        }
+    
+        if (details[i].attibutes[k].ssgf === true) {
+          newObj.attibutes[2] = true;
+        }
+    
+        if (details[i].attibutes[k].ism === true) {
+          newObj.attibutes[3] = true;
+        }
+        
+        if (details[i].attibutes[k].fyp === true) {
+          newObj.attibutes[4] = true;
+        }
+    
+        if (details[i].attibutes[k].year === true) {
+          newObj.attibutes[5] = true;
+        }
+    
+        if (details[i].attibutes[k].grsu === true) {
+          newObj.attibutes[6] = true;
+        }
+    
+        if (details[i].attibutes[k].lab === true) {
+          newObj.attibutes[7] = true;
+        }
+    
+        if (details[i].attibutes[k].urop === true) {
+          newObj.attibutes[8] = true;
+        }
+    
+      }
+    }
+  
+    var tempSemData = [];
+    if (details[i].semesterData !== undefined) {
+      for (var j = 0; j < details[i].semesterData.length; j++) {
+        // console.log("inside j (" + j + "): " + details[i].semesterData[j].semester);
+        tempSemData.push(details[i].semesterData[j].semester);
+      }
+    }
+    newObj.semester = tempSemData;
+  
+    console.log(newObj);
+    finalArr.push(newObj);
+  }
+  
+  fs.writeFile('out.json', JSON.stringify(finalArr, null, 4), function(err, result) {
+    if (err) {
+      console.log(err);
+    }
+  });
 }
 
-var finalArr = [];
+if (checking === true) {
+  var max = 0;
 
-for (var i = 0; i < details.length; i++ ){
-  // console.log(details[i]);
-  var newObj = {...templateObj};
-
-  newObj.moduleCode = details[i].moduleCode;
-  newObj.title = details[i].title;
-  newObj.moduleCredit = parseInt(details[i].moduleCredit);
-  newObj.department = details[i].department;
-  newObj.faculty = details[i].faculty;
-  
-  if (details[i].preclusion !== undefined) {
-    newObj.preclusion = details[i].preclusion;
-  }
-
-  if (details[i].prerequisite !== undefined) {
-    newObj.prerequisite = details[i].prerequisite;
-  }
-  
-  if (details[i].corequisite !== undefined) {
-    newObj.corequisite = details[i].corequisite;
-  }
-  
-  if (details[i].attibutes !== undefined) {
-    for (var k = 0; k < details[i].attibutes.length; k++) {
-      if (details[i].attibutes[k].su === true) {
-        newObj.attibutes[0] = true;
-      }
-  
-      if (details[i].attibutes[k].sfs === true) {
-        newObj.attibutes[1] = true;
-      }
-  
-      if (details[i].attibutes[k].ssgf === true) {
-        newObj.attibutes[2] = true;
-      }
-  
-      if (details[i].attibutes[k].ism === true) {
-        newObj.attibutes[3] = true;
-      }
-      
-      if (details[i].attibutes[k].fyp === true) {
-        newObj.attibutes[4] = true;
-      }
-  
-      if (details[i].attibutes[k].year === true) {
-        newObj.attibutes[5] = true;
-      }
-  
-      if (details[i].attibutes[k].grsu === true) {
-        newObj.attibutes[6] = true;
-      }
-  
-      if (details[i].attibutes[k].lab === true) {
-        newObj.attibutes[7] = true;
-      }
-  
-      if (details[i].attibutes[k].urop === true) {
-        newObj.attibutes[8] = true;
-      }
-  
+  for (var i = 0; i < data.length; i++) {
+    // console.log(data[i].moduleCode);
+    if(data[i].moduleCode.length > max) {
+      console.log(data[i].moduleCode);
+      max = data[i].moduleCode.length;
     }
   }
 
-  var tempSemData = [];
-  if (details[i].semesterData !== undefined) {
-    for (var j = 0; j < details[i].semesterData.length; j++) {
-      // console.log("inside j (" + j + "): " + details[i].semesterData[j].semester);
-      tempSemData.push(details[i].semesterData[j].semester);
-    }
-  }
-  newObj.semester = tempSemData;
-
-  console.log(newObj);
-  finalArr.push(newObj);
+  console.log(max);
 }
-
-fs.writeFile('out.json', JSON.stringify(finalArr, null, 4), function(err, result) {
-  if (err) {
-    console.log(err);
-  }
-});
