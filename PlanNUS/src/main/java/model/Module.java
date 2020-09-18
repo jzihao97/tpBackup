b2781514d3503ca1e1386c9439d5de2ca88f99b3
JuZihao <57383789+JuZihao@users.noreply.github.com>
2020-09-18 18:03:27 +0800

    Minor debuging

--- PlanNUS/src/main/java/model/Module.java
+++ PlanNUS/src/main/java/model/Module.java
@@ -86,8 +86,7 @@ public class Module implements Comparable<Module>{
         case "CU":           //Completed Unsatisfactorily
             //Fallthrough
         case "S":            //Satisfactory
-            this.CAP = -1.00;
-            break;
+            //Fallthrough
         case "U":            //Unsatisfactory
             //Fallthrough
         case "W":            //Withdrawn
@@ -103,6 +102,8 @@ public class Module implements Comparable<Module>{
         case "EXE":         //Exempted
             //Fallthrough
         case "NT":           //Not taken
+            this.CAP = -1.00;
+            break;
         default:
             this.CAP = 0.00;
             break;
